dd57744c3fe0b0021f76d5a004831d648b6bbbe9
sahilcalyx <sahilsawantcalyx@gmail.com>
2025-12-05 14:38:24 +0530

    neww

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,11 @@ import dynamic from "next/dynamic";
 import RouteSeo from "./RouteSeo";
 const App = dynamic(() => import("../react/App"), { ssr: false });
 
+export async function generateMetadata() {
+  const { getSeoForPath } = await import("../seo/seo.config");
+  return getSeoForPath("/");
+}
+
 export default function Home() {
   return (
     <>
